d5d1d6d6a8fc3b0f07d67b22da662c65740ab174
Noel Pinto <noelpinto47@gmail.com>
2025-01-15 10:17:06 +0530

    updated code for proj

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_playground/src/features/home/presentation/screens/home_page.dart';
+import 'package:flutter_playground/playground_controller.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -12,13 +12,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final PlaygroundController controller = Get.put(PlaygroundController());
     return GetMaterialApp(
       title: 'Flutter Playground',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Playground'),
+          backgroundColor: Theme.of(context).appBarTheme.backgroundColor, 
+          elevation: 4.0,
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          color: Theme.of(context).colorScheme.surface,
+          child: Obx(
+            () => Text(
+              controller.count.value.toString(),
+              style: TextStyle(
+                fontSize: 16.0
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).colorScheme.surfaceDim,
+          child: const Icon(Icons.add),
+          onPressed: () {
+            controller.increment();
+          },
+        ),
+      ),
     );
   }
 }
